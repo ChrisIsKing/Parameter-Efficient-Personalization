ed7d94720eb388eb33d5768bcc35e394f434a317
Stefan Heng <stefan.hg@outlook.com>
2023-05-30 16:56:27 -0400

    Fix: dataset prepare deteministic; train log file write; unhealthyconversations label map

--- peft_u/write_data/prepare_tweeteval.py
+++ peft_u/write_data/prepare_tweeteval.py
@@ -12,15 +12,38 @@ logger = get_logger('TweetEval Prep')
 if __name__ == '__main__':
     from stefutil import *
 
-    # Labels: [`Hateful`, `Non-hateful`]
     dset_base_path = os_join(u.proj_path, u.dset_dir, 'tweeteval')
-    data, headers = load_csv(os_join(dset_base_path, 'annotations_g3.csv'), delimiter=",", header=True)
 
-    user_data: PersonalizedData = defaultdict(dict)
-    for row in data:
-        post_id, text, user_labels = row[0], row[1], row[2:]
-        for uid, label in enumerate(user_labels):
-            user_data[uid][post_id] = dict(text=text, label=[label])
+    def run():
+        # Labels: [`Hateful`, `Non-hateful`]
+        data, headers = load_csv(os_join(dset_base_path, 'annotations_g3.csv'), delimiter=",", header=True)
 
-    save_datasets(data=user_data, base_path=dset_base_path)
-    mic(data2label_meta(data=user_data))
+        user_data: PersonalizedData = defaultdict(dict)
+        for row in data:
+            post_id, text, user_labels = row[0], row[1], row[2:]
+            for uid, label in enumerate(user_labels):
+                user_data[uid][post_id] = dict(text=text, label=[label])
+
+        save_datasets(data=user_data, base_path=dset_base_path)
+        mic(data2label_meta(data=user_data))
+    run()
+
+    def check_deterministic(fnm: str = None):
+        import json
+
+        path_old = f'{fnm}-old.json'
+        with open(os_join(dset_base_path, path_old), 'r') as f:
+            dset_ori = json.load(f)
+
+        path_new = f'{fnm}.json'
+        with open(os_join(dset_base_path, path_new), 'r') as f:
+            dset_new = json.load(f)
+
+        def get_uid2split_sizes(dset):
+            return {uid: {split: len(samples) for split, samples in dset[uid].items()} for uid in dset}
+
+        mic(get_uid2split_sizes(dset_ori))
+        mic(get_uid2split_sizes(dset_new))
+        assert dset_ori == dset_new
+    # check_deterministic(fnm='user_data_no_leak')
+    # check_deterministic(fnm='user_data_leaked')
--- peft_u/write_data/prepare_unhealthyconversations.py
+++ peft_u/write_data/prepare_unhealthyconversations.py
@@ -11,14 +11,21 @@ if __name__ == '__main__':
     from stefutil import *
 
     dset_base_path = os_join(u.proj_path, u.dset_dir, 'unhealthyconversations')
-
     data, headers = load_csv(os_join(dset_base_path, 'unhealthy_full.csv'), delimiter=',', header=True)
 
+    label_map = {'1': 'healthy', '0': 'unhealthy'}
     user_data = defaultdict(dict)
 
     for row in tqdm(data, desc='Processing data'):
         post_id, text, user_id, label = row[0], row[1], row[3], row[9]
         if post_id not in user_data[user_id]:
-            user_data[user_id][post_id] = dict(text=text, label=[label])
+            user_data[user_id][post_id] = dict(text=text, label=[label_map[label]])
     save_datasets(data=user_data, base_path=dset_base_path)
     mic(data2label_meta(data=user_data))
+
+    def check_healthy_counts():
+        import pandas as pd
+
+        df = pd.read_csv(os_join(dset_base_path, 'unhealthy_full.csv'))
+        mic(df['healthy'].value_counts())
+    # check_healthy_counts()
