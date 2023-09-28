ae37969fb68d9a54ed75a021db9cc2b29755eb04
Christopher Clarke <t-cclarke@microsoft.com>
2023-09-28 14:27:40 -0700

    Added flag to change desired data output path

--- peft_u/write_data/prepare_unhealthyconversations.py
+++ peft_u/write_data/prepare_unhealthyconversations.py
@@ -1,14 +1,22 @@
 from os.path import join as os_join
 from collections import defaultdict
-
+from argparse import ArgumentParser
 from tqdm import tqdm
-
 from peft_u.util import *
 from peft_u.preprocess.convert_data_format import *
 
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('--output_dir', '-o', default=None, type=str, help='Path to output directory')
+    return parser.parse_args()
 
 if __name__ == '__main__':
     from stefutil import *
+
+    args = parse_args()
+    if args.output_dir is not None:
+        output_dir = os_join(args.output_dir, 'unhealthyconversations')
+        make_dirs(output_dir)
 
     dset_base_path = os_join(u.proj_path, u.dset_dir, 'unhealthyconversations')
 
@@ -22,7 +30,7 @@ if __name__ == '__main__':
             post_id, text, user_id, label = row[0], row[1], row[3], row[9]
             if post_id not in user_data[user_id]:
                 user_data[user_id][post_id] = dict(text=text, label=[label_map[label]])
-        save_datasets(data=user_data, base_path=dset_base_path)
+        save_datasets(data=user_data, base_path=output_dir if args.output_dir is not None else dset_base_path)
         mic(data2label_meta(data=user_data))
     run()
 
